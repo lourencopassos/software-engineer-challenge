7ba4ac7c6ef0fcd5b15db55b1083aa97ca86a3db
Lourenco <lo.passos93@gmail.com>
2021-03-20 20:55:05 -0300

    feat: add index to application

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { AddressInfo } from "net";
+import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors"
+
+dotenv.config();
+const app = express();
+app.use(cors({ origin: true }));
+
+app.use(express.json());
+
+export const server = app.listen(3000, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server running: http://localhost:${address.port}`);
+  } else {
+    console.error(`Server running failed`);
+  }
+});
