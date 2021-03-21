6e7ae00b1220fa643861b0897c265e6184ff13d6
Lourenco <lo.passos93@gmail.com>
2021-03-21 17:28:36 -0300

    refactor: remove unused code

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { AddressInfo } from "net";
 import dotenv from 'dotenv';
 import express from "express";
 import cors from "cors"
+import fs from 'fs';
+
 
 dotenv.config();
 const app = express();
@@ -11,6 +13,7 @@ app.use(express.json());
 
 export const server = app.listen(3000, () => {
   if (server) {
+
     const address = server.address() as AddressInfo;
     console.log(`Server running: http://localhost:${address.port}`);
   } else {
