c5f560fa35280421d66b3d1cdb31ae9f67b86c5f
Lourenco <lo.passos93@gmail.com>
2021-03-21 23:06:29 -0300

    refactor: add tables for priorities, add new scripts and copy the txt files to docker container

--- docker/db/scripts.sql
+++ docker/db/scripts.sql
@@ -7,3 +7,18 @@ username VARCHAR(50)
 LOAD DATA LOCAL INFILE  '/var/lib/mysql-files/users.csv' into table Users
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r';
+
+
+CREATE TABLE Priority_One (
+id VARCHAR(36)
+);
+
+LOAD DATA LOCAL INFILE  '/var/lib/mysql-files/lista_relevancia_1.txt' into table Priority_One
+FIELDS TERMINATED BY '\n' OPTIONALLY ENCLOSED BY '"';
+
+CREATE TABLE Priority_Two (
+id VARCHAR(36)
+);
+
+LOAD DATA LOCAL INFILE  '/var/lib/mysql-files/lista_relevancia_2.txt' into table Priority_Two
+FIELDS TERMINATED BY '\n' OPTIONALLY ENCLOSED BY '"';
