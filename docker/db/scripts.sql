1b09f76285fb87d66122c07a771e3f8a57d5c237
Lourenco <lo.passos93@gmail.com>
2021-03-24 20:39:57 -0300

    feat: add sql script for auth table

--- docker/db/scripts.sql
+++ docker/db/scripts.sql
@@ -22,3 +22,12 @@ id VARCHAR(36)
 
 LOAD DATA LOCAL INFILE  '/var/lib/mysql-files/lista_relevancia_2.txt' into table Priority_Two
 FIELDS TERMINATED BY '\r\n' OPTIONALLY ENCLOSED BY '"';
+
+
+CREATE TABLE Auth (
+id VARCHAR(36),
+email varchar(36),
+password varchar(100)
+);
+
+INSERT INTO Auth (id, email,password) VALUES ('123456', 'challenge@picpay.com.br', '$2y$10$apzE9irks1Yp.M8Ou84eXOrZzV7LNzVNfokIeLdpnJP3UqbAdbTwm')
