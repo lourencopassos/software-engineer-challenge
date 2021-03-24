922b05be2dc143ee903853048dfb7898df0314d4
Lourenco <lo.passos93@gmail.com>
2021-03-24 17:26:48 -0300

    refactor: change line break in tables

--- docker/db/scripts.sql
+++ docker/db/scripts.sql
@@ -5,8 +5,8 @@ username VARCHAR(50)
 );
 
 LOAD DATA LOCAL INFILE  '/var/lib/mysql-files/users.csv' into table Users
-FIELDS TERMINATED BY ',' ENCLOSED BY '"'
-LINES TERMINATED BY '\r';
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n';
 
 
 CREATE TABLE Priority_One (
@@ -14,11 +14,11 @@ id VARCHAR(36)
 );
 
 LOAD DATA LOCAL INFILE  '/var/lib/mysql-files/lista_relevancia_1.txt' into table Priority_One
-FIELDS TERMINATED BY '\n' OPTIONALLY ENCLOSED BY '"';
+FIELDS TERMINATED BY '\r\n' OPTIONALLY ENCLOSED BY '"';
 
 CREATE TABLE Priority_Two (
 id VARCHAR(36)
 );
 
 LOAD DATA LOCAL INFILE  '/var/lib/mysql-files/lista_relevancia_2.txt' into table Priority_Two
-FIELDS TERMINATED BY '\n' OPTIONALLY ENCLOSED BY '"';
+FIELDS TERMINATED BY '\r\n' OPTIONALLY ENCLOSED BY '"';
